96e666129e7b6161255c01e55714c4315019503c
Victor JEMAN <victorjeman@gmail.com>
2022-12-28 18:10:35 +0200

    Added mobx store to the app

--- src/components/actors/actors.tsx
+++ src/components/actors/actors.tsx
@@ -1,44 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
 
 import { NOTIFICATION_ACTION_TYPE, NOTIFICATION_TYPE } from 'constants/general.constants'
 import { IActor } from 'models/common.models'
 import { useNotification } from 'services/notification-provider/notification-provider'
-import { deleteActorByIdAPI, addActorAPI, editActorAPI } from 'services/actor-service/actor.service'
-import { useActors } from 'hooks/use-actors'
+import { useStore } from 'store/root.store'
 
 import { ActorThumbnail } from 'components/actor-thumbnail/actor-thumbnail'
 import { ActorForm } from 'components/actor-form/actor-form'
 import { Button } from 'components/button/button'
 import { Modal } from 'components/modal/modal'
 
-export const Actors = () => {
-	const [showModal, setShowModal] = useState<boolean>(false)
-	const [actorToEdit, setActorToEdit] = useState<IActor | undefined>()
-	const [actors, setActors] = useState<IActor[]>([])
-
-	const initialActors = useActors()
+export const Actors = observer(() => {
 	const dispatch = useNotification()
 
+	const { actorStore } = useStore()
+	const { actors, showModal, actorToUpdate } = actorStore
+
 	useEffect(() => {
-		setActors(initialActors)
-	}, [initialActors])
+		actorStore.fetchActors()
+	}, [])
 
-	const deleteActor = async (id: number) => {
-		const { success, data } = await deleteActorByIdAPI(id, actors)
+	const handleActorSubmit = async (actor: IActor) => {
+		const { success } = await actorStore.handleActorSubmit(actor)
 
-		setActors(data)
-
-		dispatch({
-			actionType: NOTIFICATION_ACTION_TYPE.ADD_NOTIFICATION,
-			type: success ? NOTIFICATION_TYPE.SUCCESS : NOTIFICATION_TYPE.DANGER,
-			message: success ? 'Actor successfully deleted!' : "Wasn't able to delete the actor",
-		})
-	}
-
-	const addActor = async (actor: IActor) => {
-		const { data, success } = await addActorAPI(actor, actors)
-
-		setActors(data)
 		setShowModal(false)
 
 		dispatch({
@@ -48,17 +33,10 @@ export const Actors = () => {
 		})
 	}
 
-	const showFormActorInEditMode = async (id: number | undefined) => {
-		const actorToEdit = actors.find((actor) => actor.id === id)
-		setActorToEdit(actorToEdit)
-		setShowModal(true)
-	}
+	const handleActorUpdate = async (actorToUpdate: IActor) => {
+		const { success } = await actorStore.handleActorUpdate(actorToUpdate)
 
-	const editActor = async (actorToEdit: IActor) => {
-		const { data, success } = await editActorAPI(actorToEdit, actors)
-
-		setActors(data)
-		setShowModal(false)
+		actorStore.setShowModal(false)
 
 		dispatch({
 			actionType: NOTIFICATION_ACTION_TYPE.ADD_NOTIFICATION,
@@ -67,28 +45,51 @@ export const Actors = () => {
 		})
 	}
 
-	const submit = (actor: IActor) => {
-		if (actorToEdit) editActor(actor)
-		else addActor(actor)
+	const handleActorDelete = async (actor: IActor) => {
+		const { success } = await actorStore.handleActorDelete(actor)
+
+		dispatch({
+			actionType: NOTIFICATION_ACTION_TYPE.ADD_NOTIFICATION,
+			type: success ? NOTIFICATION_TYPE.SUCCESS : NOTIFICATION_TYPE.DANGER,
+			message: success ? 'Actor successfully deleted!' : "Wasn't able to delete the actor",
+		})
+	}
+
+	const handleActorFormSubmit = (actor: IActor) => {
+		if (actorToEdit) handleActorUpdate(actor)
+		else handleActorSubmit(actor)
+	}
+
+	const showFormActorInEditMode = async (actorToUpdate: IActor) => {
+		const actorToEdit = actors.find((actor) => actor.id === actorToUpdate.id)
+		actorStore.setActorToUpdate(actorToEdit)
+		actorStore.setShowModal(true)
 	}
 
 	return (
 		<section>
 			{actors?.map((actor) => (
 				<div key={actor.nanoid}>
-					<ActorThumbnail actor={actor} onDelete={deleteActor} onEdit={showFormActorInEditMode} />
+					<ActorThumbnail
+						actor={actor}
+						onDelete={handleActorDelete}
+						onEdit={showFormActorInEditMode}
+					/>
 				</div>
 			))}
 
-			<Button size={'xl'} isFullWidth={true} onClick={() => setShowModal(true)}>
+			<Button size={'xl'} isFullWidth={true} onClick={() => actorStore.setShowModal(true)}>
 				Add actor
 			</Button>
 
-			<Modal isOpen={showModal} title='Add new actor' onClose={() => setShowModal(false)}>
+			<Modal
+				isOpen={showModal}
+				title='Add new actor'
+				onClose={() => actorStore.setShowModal(false)}>
 				<>
-					<ActorForm onSubmit={submit} actorToEdit={actorToEdit} />
+					<ActorForm onSubmit={handleActorFormSubmit} actorToEdit={actorToUpdate} />
 				</>
 			</Modal>
 		</section>
 	)
-}
+})
